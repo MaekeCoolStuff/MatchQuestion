37b6c39772af0fdddaf907d8f085884d105239df
Mae van Dijk <mae.vandijk1990@gmail.com>
2019-03-24 10:44:45 +0100

    more refactoring

--- matchvraag2/src/match-collision-service.ts
+++ matchvraag2/src/match-collision-service.ts
@@ -6,12 +6,12 @@ export class MatchCollisionService {
     public static matchContainers: MatchContainer[] = [];
     public static matchItemContainers: MatchItemContainer[] = [];
 
-    public static checkForContainerCollisions(x, y, matchItem: MatchItem): boolean {
+    public static checkForContainerCollisions(x: number, y: number, matchItem: MatchItem): boolean {
         let hasCollisions: boolean = false;
         let allContainersToCheck: any[] = <any[]>MatchCollisionService.matchContainers
             .concat(<any[]>MatchCollisionService.matchItemContainers)
         
-            allContainersToCheck.forEach(c => {
+        allContainersToCheck.forEach(c => {
             let isInDropzone = c.getBounds().contains(x, y);
             if(isInDropzone) {
                 hasCollisions = true;
